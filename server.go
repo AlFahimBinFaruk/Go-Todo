a2daf26b075462f00b0854986ff1cd0c73a1983d
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-06-23 13:40:56 +0600

    feat:db and environment setup done

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"go-todo/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "PONG",
+		})
+	})
+	router.Run()
+}
