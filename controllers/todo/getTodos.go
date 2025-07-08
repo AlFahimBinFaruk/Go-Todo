e0a64c0266dc12cc3beea351a9a327606e33b29f
AlFahimBinFaruk <alfahim.ps@gmail.com>
2025-07-08 13:04:08 +0600

    feat:GetTodos updated

--- controllers/todo/getTodos.go
+++ controllers/todo/getTodos.go
@@ -3,20 +3,36 @@ package todo
 import (
 	"go-todo/initializers"
 	"go-todo/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func GetTodos(c *gin.Context) {
-	var todos []models.TODO
-	result := initializers.DB.Find(&todos)
-	if result.Error != nil {
-		c.JSON(400, gin.H{
-			"msg": "failed to get todo!!",
-		})
+	defer func() {
+		if r := recover(); r != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": "Interner Server Error in => GetTodos.",
+			})
+		}
+	}()
+
+	userIdStr, _ := c.Get("userId")
+	userId, err := uuid.Parse(userIdStr.(string))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Bad Request."})
 		return
 	}
-	c.JSON(200, gin.H{
-		"todos": todos,
+
+	var user models.USER
+	exixts := initializers.DB.Preload("Todos").First(&user, "id = ?", userId)
+	if exixts.Error != nil {
+		c.AbortWithStatusJSON(http.StatusNoContent, gin.H{"error": "No user found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"todos": user.Todos,
 	})
+
 }
